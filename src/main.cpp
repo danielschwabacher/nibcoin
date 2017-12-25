39339b3308d9fcae7ce9d5b42f1486c09fcc89c5
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-25 00:36:46 -0500

    simpler serialization and deserialization prints.

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,17 @@
 #include "block.h"
 #include "blockchain.h"
+#include "Serialization.h"
 #include <iostream>
-#include <fstream>
 
 int main(){ 
     Blockchain primary_blockchain;   
     primary_blockchain.new_block("First block");
     primary_blockchain.new_block("Second block");
     primary_blockchain.new_block("Third block");
-    primary_blockchain.print_blockchain();    
-    
-    Block test_block("previous", "hello");
-
-    std::ofstream ofs("/tmp/filename.dat", std::ios::binary);
-    {
-        boost::archive::binary_oarchive oa(ofs);
-        oa << test_block;
-    }
-
-    {
-      std::ifstream file{"/tmp/filename.dat"};
-      boost::archive::binary_iarchive ia(file);
-      Block other_block("s");
-      ia >> other_block;
-      std::cout << "Deserialized block has previous hash: " << other_block.get_prev_hash() << '\n';      
-      std::cout << "Deserialized block is: " << other_block.get_data() << '\n';
-    }
-
-
-
+    primary_blockchain.print_blockchain();
+    SerializationWrapper serial;
+    DeserializationWrapper deserial;
+    serial.demonstrate_serialization();
+    deserial.demonstrate_deserialization();
     return 0;
 }
