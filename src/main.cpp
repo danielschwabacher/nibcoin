4db3a2418466e03e3d36096c418bb658c0d375d6
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-24 23:02:01 -0500

    block serialization and deserialization example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "block.h"
 #include "blockchain.h"
 #include <iostream>
+#include <fstream>
 
 int main(){ 
     Blockchain primary_blockchain;   
@@ -8,5 +9,25 @@ int main(){
     primary_blockchain.new_block("Second block");
     primary_blockchain.new_block("Third block");
     primary_blockchain.print_blockchain();    
+    
+    Block test_block("previous", "hello");
+
+    std::ofstream ofs("/tmp/filename.dat", std::ios::binary);
+    {
+        boost::archive::binary_oarchive oa(ofs);
+        oa << test_block;
+    }
+
+    {
+      std::ifstream file{"/tmp/filename.dat"};
+      boost::archive::binary_iarchive ia(file);
+      Block other_block("s");
+      ia >> other_block;
+      std::cout << "Deserialized block has previous hash: " << other_block.get_prev_hash() << '\n';      
+      std::cout << "Deserialized block is: " << other_block.get_data() << '\n';
+    }
+
+
+
     return 0;
 }
