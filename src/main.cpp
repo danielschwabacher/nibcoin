cbf3fb790e2fff87151fe39883e41f5639059740
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-28 01:40:21 -0500

    removed extra demonstrations.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 #include "block.h"
 #include "blockchain.h"
 #include "Serialization.h"
+#include "../lib/json.hpp"
 #include <iostream>
 #include <leveldb/db.h>
 
+
+/*
+
+*/
 int main(){ 
     Blockchain primary_blockchain;
-    Block test_block("previous_hash", "random_block_data");
-    SerializationWrapper serial;
-    DeserializationWrapper deserial;
-    nlohmann::json serialized_block_data = serial.serialize_block(test_block);
-    std::cout<<"JSON data is: "<<serialized_block_data<<std::endl;
-    Block restored_block = deserial.deserialize_block(serialized_block_data);
-    std::cout<<"Restored block DATA is: "<<restored_block.get_data()<<std::endl;
-    std::cout<<"Restored block TIMESTAMP is: "<<restored_block.get_timestamp()<<std::endl;
-    std::cout<<"Restored block PREVIOUS HASH is: "<<restored_block.get_prev_hash()<<std::endl;
-    std::cout<<"Restored block BLOCK HASH is: "<<restored_block.get_block_hash()<<std::endl;
     return 0;
 }
 
