c6ad15491bd7b7eaafb6e083ec488982f20dcf12
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-28 01:24:01 -0500

    serialization/deserialization demo.

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,17 @@
 #include <leveldb/db.h>
 
 int main(){ 
-    Blockchain primary_blockchain;   
-    primary_blockchain.new_block("First block");
-    primary_blockchain.new_block("Second block");
-    primary_blockchain.new_block("Third block");
-    primary_blockchain.print_blockchain();
+    Blockchain primary_blockchain;
+    Block test_block("previous_hash", "random_block_data");
     SerializationWrapper serial;
     DeserializationWrapper deserial;
-    serial.demonstrate_serialization();
-    deserial.demonstrate_deserialization();
-    // ----------------
-    leveldb::DB* db;
-    leveldb::Options options;
-    options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "/tmp/testdb", &db);
-    if (status.ok()){
-        std::cout<<"Database is okay"<<std::endl;
-    }
+    nlohmann::json serialized_block_data = serial.serialize_block(test_block);
+    std::cout<<"JSON data is: "<<serialized_block_data<<std::endl;
+    Block restored_block = deserial.deserialize_block(serialized_block_data);
+    std::cout<<"Restored block DATA is: "<<restored_block.get_data()<<std::endl;
+    std::cout<<"Restored block TIMESTAMP is: "<<restored_block.get_timestamp()<<std::endl;
+    std::cout<<"Restored block PREVIOUS HASH is: "<<restored_block.get_prev_hash()<<std::endl;
+    std::cout<<"Restored block BLOCK HASH is: "<<restored_block.get_block_hash()<<std::endl;
     return 0;
 }
+
