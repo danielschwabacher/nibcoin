ea2e1d66f56f2f5a70d3692ae7ec7c25dfe5b8fd
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-29 21:49:31 -0500

    iteration demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "block.h"
 #include "blockchain.h"
 #include "Serialization.h"
+#include "chain_iterator.h"
 #include "../lib/json.hpp"
 #include <iostream>
 #include <leveldb/db.h>
@@ -13,6 +14,9 @@ int main(){
     Blockchain primary_blockchain;
     primary_blockchain.new_block("First block");
     primary_blockchain.new_block("Second block");
+    leveldb::DB *primary_database = primary_blockchain.get_database();
+    BlockchainIterator printer(primary_database);
+    printer.print_all_kv();
     return 0;
 }
 
