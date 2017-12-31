1f99d4721674c95419fe591656ae5d12420d88d1
Daniel Schwabacher <danielschwabacher@gmail.com>
2017-12-30 22:49:30 -0500

    interactive demos with CommandDispatcher.

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
+#include <iostream>
+#include <leveldb/db.h>
 #include "block.h"
 #include "blockchain.h"
 #include "Serialization.h"
 #include "chain_iterator.h"
+#include "command_dispatcher.h"
 #include "../lib/json.hpp"
-#include <iostream>
-#include <leveldb/db.h>
-
 
 /*
 
 */
 int main(){ 
     Blockchain primary_blockchain;
-    primary_blockchain.new_block("First block");
-    primary_blockchain.new_block("Second block");
-    leveldb::DB *primary_database = primary_blockchain.get_database();
-    BlockchainIterator printer(primary_database);
-    printer.print_all_kv();
+    // primary_blockchain.new_block("First block");
+    // primary_blockchain.new_block("Second block");
+    // leveldb::DB *primary_database = primary_blockchain.get_database();
+    // BlockchainIterator printer(primary_database);
+    // printer.print_all_kv();
+    CommandDispatcher dispatch(&primary_blockchain);
+    std::string command;
+    while (true){
+        std::cout<<"Enter a command: ";
+        std::cin >> command;
+        std::cout<<"Entered: " << command << std::endl;
+        if (command == "stop")
+            exit(0);
+        else if (command == "exit")
+            exit(0);
+        else if (command == "quit")
+            exit(0);
+        else if (command == "add_block")
+            dispatch.run_add_block();
+        else if (command == "print_chain")
+            dispatch.run_print_chain();
+        else{
+            std::cout<<"Invalid input, try again"<<std::endl;
+        }
+    }
     return 0;
 }
-
