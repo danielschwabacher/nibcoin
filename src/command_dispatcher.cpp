26dfa852408dd6d6eacf8aeb90ee8712e43a7427
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-01-02 22:58:46 -0500

    print_chain option.

--- src/command_dispatcher.cpp
+++ src/command_dispatcher.cpp
@@ -37,7 +37,10 @@ int CommandDispatcher::run_add_block(){
     return 0;
 }
 
-int CommandDispatcher::run_print_chain(){
+/*
+    Print out the entire database structure, including internal keys. 
+*/
+int CommandDispatcher::run_dump_chain(){
     // The first get_database call returns the Database object in the Blockchain context
     // The second one returns the actual leveldb pointer
     leveldb::DB *curr_db = chain_context->get_database().get_database();
@@ -47,17 +50,26 @@ int CommandDispatcher::run_print_chain(){
 }
 
 int CommandDispatcher::run_delete_chain(){
-    Database curr_db = chain_context->get_database();    
+    Database curr_db = chain_context->get_database();
     system("rm -rf /tmp/blocks");
+    curr_db.delete_database();
     std::cout<<"Database and block chain deleted!"<<std::endl;
     exit(0);
+}
+
+int CommandDispatcher::run_pretty_print(){
+    leveldb::DB *curr_db = chain_context->get_database().get_database();
+    BlockchainIterator printer(curr_db);
+    printer.pretty_print();
+    return 0;
 }
 
 int CommandDispatcher::print_help(){
     std::cout<<"---Supported Commands---"<<std::endl;
     std::cout<<"<add_block>: Add a new block to the blockchain"<<std::endl;
     std::cout<<"<delete_chain>: Deletes the blockchain and the associated data"<<std::endl;
-    std::cout<<"<print_chain>: Prints out the blockchain structure."<<std::endl;
+    std::cout<<"<dump_chain>: Print out the entire database structure, including internal keys."<<std::endl;
+    std::cout<<"<print_chain>: Nicely display the blockchain contents"<<std::endl;
     std::cout<<"<help>: Show this help menu."<<std::endl;
     return 0;
 }
