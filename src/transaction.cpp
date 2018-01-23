44502d2a20e715dc8476922ac836c9e721c214ec
Daniel Schwabacher <danielschwabacher@gmail.com>
2018-01-23 00:46:36 -0700

    adding base constructors for transaction types. Adding coinbase transactions.

---
+++ src/transaction.cpp
@@ -0,0 +1,33 @@
+#include "transaction.h"
+
+const int SUBSIDY = 10;
+/*
+*/
+Transaction::Transaction(std::string id, std::vector<TransactionInput> inputs, std::vector<TransactionOutput> outputs){
+    transaction_id = id;
+    transaction_inputs = inputs;
+    transaction_outputs = outputs;
+}
+
+TransactionOutput::TransactionOutput(int v, std::string pub_key){
+    value = v;
+    public_key = pub_key;
+}
+
+TransactionInput::TransactionInput(std::string prev_output_txid, std::string sig, int v_index){
+    tx_id = prev_output_txid;
+    script_signature = sig;
+    vout = v_index;
+}
+
+
+Transaction *Transaction::new_coinbase_tx(std::string to, std::string data){
+    TransactionInput txin("null", data, -1);
+    TransactionOutput txout(SUBSIDY, to);
+    std::vector<TransactionInput> inputs_vector;
+    std::vector<TransactionOutput> outputs_vector;
+    inputs_vector.push_back(txin);
+    outputs_vector.push_back(txout);
+    static Transaction cb_transaction = Transaction("coinbase_tx", inputs_vector, outputs_vector);
+    return &cb_transaction;
+}
---
+++ src/transaction.h
@@ -0,0 +1,45 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+#include "block.h"
+#include "serialization.h"
+
+#ifndef TX_INC
+#define TX_INC
+
+/*
+*/
+class TransactionOutput{
+    private:
+        int value;
+        std::string public_key;
+    public:
+        TransactionOutput(int v, std::string pub_key);
+};
+
+/*
+*/
+class TransactionInput{
+    private:
+        std::string tx_id;
+        std::string script_signature;
+        int vout;
+    public:
+        TransactionInput(std::string prev_output_txid, std::string sig, int v_index);
+};
+
+/*
+*/
+class Transaction{
+    private:
+        std::string transaction_id;
+        std::vector<TransactionInput> transaction_inputs;
+        std::vector<TransactionOutput> transaction_outputs;
+    public:
+        Transaction(std::string id, std::vector<TransactionInput> inputs, std::vector<TransactionOutput> outputs);
+        // A coinbase TX only has input
+        Transaction *new_coinbase_tx(std::string to, std::string data);
+};
+
+#endif
